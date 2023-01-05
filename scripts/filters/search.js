a71d7e2da11cd62fd47dfaf9f522746eb7b53c15
Matthieu Mary <matthieu.mary2@gmail.com>
2023-01-05 17:27:57 +0100

    remove same function than on methods branch

--- scripts/filters/search.js
+++ scripts/filters/search.js
@@ -9,21 +9,10 @@ function filterRecipesBySearchInput(e) {
   dropdownLists.forEach((dropdown) => (dropdown.innerHTML = ""));
   const searchedRecipe = e.target.value.toLowerCase().trim();
   let searchResult = [];
-  // Start filtering by name, description and ingredients, only if value.length >= 3
   if (e.target.value.length >= 3) {
     searchWithNativeLoops(searchedRecipe, searchResult)
-    // Display message if no recipes founded
-    if (searchResult.length === 0) {
-      const failMessage = document.createElement("h3");
-      failMessage.classList.add("fail-message");
-      failMessage.textContent = `Aucune recette ne correspond à votre critère… vous pouvez
-        chercher "tarte aux pommes",  "poisson", etc`;
-      cardContainer.appendChild(failMessage);
-    }
-    createCard(searchResult);
-    createDropdownList(searchResult);
-    updatedRecipes(searchResult)
   } else {
+    cardContainer.style.display = "grid";
     createCard(allRecipes);
     createDropdownList(allRecipes);
     updatedRecipes(allRecipes)
--- scripts/filters/searchFuncNativeLoops.js
+++ scripts/filters/searchFuncNativeLoops.js
@@ -19,5 +19,19 @@ function searchWithNativeLoops(searchedRecipe, searchResult) {
         if (includeRecipe) {
           searchResult.push(recipe);
         }
-      }
+      }     
+      // Display message if no recipes founded
+      if (searchResult.length === 0) {
+      const failMessage = document.createElement("h3");
+      cardContainer.style.display = "flex";
+      failMessage.classList.add("fail-message");
+      failMessage.textContent = `Aucune recette ne correspond à votre critère… vous pouvez
+        chercher "tarte aux pommes",  "poisson", etc`;
+      cardContainer.appendChild(failMessage);
+    } else {
+      cardContainer.style.display = "grid";
+    }
+    createCard(searchResult);
+    createDropdownList(searchResult);
+    updatedRecipes(searchResult)
 }
