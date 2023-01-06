71184830d932cd03546766754226115d2c5058ab
Matthieu Mary <matthieu.mary2@gmail.com>
2023-01-06 11:42:23 +0100

    test

--- scripts/filters/filtersInputs.js
+++ scripts/filters/filtersInputs.js
@@ -1,65 +1,55 @@
+let filtersInputsUsed = false;
 // Functions for filters Inputs
 function filterIngredientsByInput(e) {
-    const inputValue = e.target.value.toLowerCase();
-    cardContainer.innerHTML = "";
-    filtersLists.forEach((filtersList) => (filtersList.innerHTML = ""));
-    if (inputValue.length >= 3) {
-      filteredRecipes = allRecipes.filter(recipe => {
-        let hasIngredients = false;
-        recipe.ingredients.forEach(ingredient => {
-          if(ingredient.ingredient.toLowerCase().includes(inputValue)) {
-            hasIngredients = true;
-          }
-        })
-        return hasIngredients
-      });
-      console.log(filteredRecipes)
-      createCard(filteredRecipes);
-      createDropdownList(filteredRecipes);
-    } else {
-      filteredRecipes = [...allRecipes];
-      createCard(filteredRecipes);
-      createDropdownList(filteredRecipes);
+  filtersInputsUsed = true;
+  const inputValue = e.target.value;
+  filtersLists[0].innerHTML = "";
+  if (inputValue.length >= 3) {
+    let ingredients = getIngredients(allRecipes);
+    allIngredients.splice(0, allIngredients.length);
+    filtersLists[0].innerHTML = "";
+    for (const ingredient of ingredients) {
+      if (ingredient.toLowerCase().includes(e.target.value.toLowerCase())) {
+        allIngredients.push(ingredient);  
+        createIngredientsDropdown(allIngredients)
+      }
     }
+  } else {
+    filtersInputsUsed = false;
+    createDropdownList(allRecipes);
   }
-  
-  function filterApplianceByInput(e) {
-    const inputValue = e.target.value.toLowerCase();
-    cardContainer.innerHTML = "";
-    filtersLists.forEach((filtersList) => (filtersList.innerHTML = ""));
-    if (inputValue.length >= 3) {
-      filteredRecipes = allRecipes.filter((recipe) =>
-        recipe.appliance.toLowerCase().includes(inputValue)
-      );
-      createCard(filteredRecipes);
-      createDropdownList(filteredRecipes);
-    } else {
-      filteredRecipes = [...allRecipes];
-      createCard(filteredRecipes);
-      createDropdownList(filteredRecipes);
+}
+
+function filterApplianceByInput(e) {
+  const inputValue = e.target.value.toLowerCase();
+  filtersLists[1].innerHTML = "";
+  if (inputValue.length >= 3) {
+    filteredRecipes = allRecipes.filter((recipe) =>
+      recipe.appliance.toLowerCase().includes(inputValue)
+    );
+    createDropdownList(filteredRecipes);
+  } else {
+    createDropdownList(allRecipes);
+  }
+}
+
+function filterUstensilsByInput(e) {
+  filtersInputsUsed = true;
+  const inputValue = e.target.value;
+  filtersLists[0].innerHTML = "";
+  console.log(filtersInputsUsed);
+  if (inputValue.length >= 3) {
+    let ustensils = getUstensils(allRecipes);
+    allUstensils.splice(0, allUstensils.length);
+    filtersLists[2].innerHTML = "";
+    for (const ustensil of ustensils) {
+      if (ustensil.toLowerCase().includes(e.target.value.toLowerCase())) {
+        allUstensils.push(ustensil);  
+        createUstensilsDropdown(allUstensils)
+      }
     }
+  } else {
+    filtersInputsUsed = false;
+    createDropdownList(allRecipes);
   }
-  
-  function filterUstensilsByInput(e) {
-    const inputValue = e.target.value.toLowerCase();
-    cardContainer.innerHTML = "";
-    filtersLists.forEach((filtersList) => (filtersList.innerHTML = ""));
-    if (inputValue.length >= 3) {
-      filteredRecipes = allRecipes.filter(recipe => {
-        let hasUstensils = false;
-        recipe.ustensils.forEach(ustensil => {
-          if(ustensil.toLowerCase().includes(inputValue)) {
-            hasUstensils = true;
-          }
-        })
-        return hasUstensils
-      });
-      console.log(filteredRecipes)
-      createCard(filteredRecipes);
-      createDropdownList(filteredRecipes);
-    } else {
-      filteredRecipes = [...allRecipes];
-      createCard(filteredRecipes);
-      createDropdownList(filteredRecipes);
-    }
-  }
+}
