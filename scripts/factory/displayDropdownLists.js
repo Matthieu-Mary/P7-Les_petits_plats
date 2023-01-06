3c98a4e30cf2b7205dcc2e3fee4e215768077e76
Matthieu Mary <matthieu.mary2@gmail.com>
2023-01-06 11:18:08 +0100

    Now filters inputs works properly

--- scripts/factory/displayDropdownLists.js
+++ scripts/factory/displayDropdownLists.js
@@ -1,157 +1,189 @@
+// GET LISTS
+let allIngredients = [];
+let allAppliances = [];
+let allUstensils = [];
+
 // CREATE LIST OF FILTERS
 function createDropdownList(recipes, currentFilter, selectedFiltersContainer) {
-    // GET LISTS
-    let allIngredients = getIngredients(recipes);
-    let allAppliances = getAppliances(recipes);
-    let allUstensils = getUstensils(recipes);
+  if (filtersInputsUsed === false) {
+    allIngredients = getIngredients(recipes);
+    allAppliances = getAppliances(recipes);
+    allUstensils = getUstensils(recipes);
+  } 
+  // Display ingredients list
+  createIngredientsDropdown(
+    allIngredients,
+    currentFilter,
+    selectedFiltersContainer
+  );
+  // Display appliances list
+  createAppliancesDropdown(
+    allAppliances,
+    currentFilter,
+    selectedFiltersContainer
+  );
+  // Display ustensils list
+  createUstensilsDropdown(
+    allUstensils,
+    currentFilter,
+    selectedFiltersContainer
+  );
+}
 
-    // Display ingredients list
-    createIngredientsDropdown(allIngredients, currentFilter, selectedFiltersContainer);
-    // Display appliances list
-    createAppliancesDropdown(allAppliances, currentFilter, selectedFiltersContainer); 
-    // Display ustensils list
-    createUstensilsDropdown(allUstensils, currentFilter, selectedFiltersContainer);
-    
-  }
+// CREATE DROPDOWN BY FILTER TYPE (INGREDIENT/APPLIANCES/USTENSILS)
 
-  // CREATE DROPDOWN BY FILTER TYPE (INGREDIENT/APPLIANCES/USTENSILS)
-
-  // ----- INGREDIENTS -----
-  function createIngredientsDropdown(ingredients, currentFilter, selectedFiltersContainer) {
-     const dropdownIngredientsList = document.querySelector(
-      ".ingredients .filter-list"
-    );
-    const ulIngredients = document.createElement("ul");
-    ingredients
-      .filter((ingredient, index) => ingredients.indexOf(ingredient) === index)
-      .forEach((ingredient) => {
-        const item = document.createElement("li");
-        item.setAttribute("onclick", "updateRecipes(event)");
-        item.textContent = ingredient;
-        ulIngredients.appendChild(item);
-        // If a filter is selected, this filter is removed from the list
-        if(currentFilter) {
-          let hasFilter = false;
-          selectedFiltersContainer.querySelectorAll("span").forEach(element => {
-            if ( element.textContent.toLowerCase() === item.textContent.toLowerCase() )
-            {
-              hasFilter = true
-              if(hasFilter) {
-                ulIngredients.removeChild(item);
-              }
-            } 
-          }) 
-        }
-      });
-    dropdownIngredientsList.appendChild(ulIngredients);
-    // Display message if no more filters disponible
-    if(ulIngredients.childElementCount === 0) {
-      const ingredientsFail = document.createElement("p");
-      ingredientsFail.classList.add("no-more-filter");
-      ingredientsFail.textContent = "Plus aucun filtre appareils disponible ..."
-      dropdownIngredientsList.removeChild(ulIngredients)
-      dropdownIngredientsList.appendChild(ingredientsFail)
-    }
-  }
-
-  //----- APPLIANCES -----
-  function createAppliancesDropdown(appliances, currentFilter, selectedFiltersContainer) {
-    const dropdownAppliancesList = document.querySelector(
-      ".appliances .filter-list"
-    );
-    const ulAppliances = document.createElement("ul");
-    appliances
-      .filter((appliance, index) => appliances.indexOf(appliance) === index)
-      .forEach((appliance) => {
-        const item = document.createElement("li");
-        item.setAttribute("onclick", "updateRecipes(event)");
-        item.textContent = appliance;
-        ulAppliances.appendChild(item);
-        // If a filter is selected, this filter is removed from the list
-        if(currentFilter) {
-          let hasFilter = false;
-          selectedFiltersContainer.querySelectorAll("span").forEach(element => {
-            if ( element.textContent.toLowerCase() === item.textContent.toLowerCase() )
-            {
-              hasFilter = true
-              if(hasFilter) {
-                ulAppliances.removeChild(item);
-              }
-            } 
-          }) 
-        }
-      });
-    dropdownAppliancesList.appendChild(ulAppliances);
-      // Display message if no more filters disponible
-      if(ulAppliances.childElementCount === 0) {
-        const applianceFail = document.createElement("p");
-        applianceFail.classList.add("no-more-filter");
-        applianceFail.textContent = "Plus aucun filtre appareils disponible ..."
-        dropdownAppliancesList.removeChild(ulAppliances)
-        dropdownAppliancesList.appendChild(applianceFail)
+// ----- INGREDIENTS -----
+function createIngredientsDropdown(
+  ingredients,
+  currentFilter,
+  selectedFiltersContainer
+) {
+  const dropdownIngredientsList = document.querySelector(
+    ".ingredients .filter-list"
+  );
+  const ulIngredients = document.createElement("ul");
+  filtersLists[0].innerHTML = "";
+  ingredients
+    .filter((ingredient, index) => ingredients.indexOf(ingredient) === index)
+    .forEach((ingredient) => {
+      const item = document.createElement("li");
+      item.setAttribute("onclick", "updateRecipes(event)");
+      item.textContent = ingredient;
+      ulIngredients.appendChild(item);
+      // If a filter is selected, this filter is removed from the list
+      if (currentFilter) {
+        let hasFilter = false;
+        selectedFiltersContainer.querySelectorAll("span").forEach((element) => {
+          if (
+            element.textContent.toLowerCase() === item.textContent.toLowerCase()
+          ) {
+            hasFilter = true;
+            if (hasFilter) {
+              ulIngredients.removeChild(item);
+            }
+          }
+        });
       }
-    }
-    
-    //----- USTENSILS -----
-  function createUstensilsDropdown(ustensils, currentFilter, selectedFiltersContainer) {
-    const dropdownUstensilsList = document.querySelector(
-      ".ustensils .filter-list"
-    );
-    const ulUstensils = document.createElement("ul");
-    ustensils
-      .filter((ustensil, index) => ustensils.indexOf(ustensil) === index)
-      .forEach((ustensil) => {
-        const item = document.createElement("li");
-        item.setAttribute("onclick", "updateRecipes(event)");
-        item.textContent = ustensil;
-        ulUstensils.appendChild(item);
-        // If a filter is selected, this filter is removed from the list
-        if(currentFilter) {
-          let hasFilter = false;
-          selectedFiltersContainer.querySelectorAll("span").forEach(element => {
-            if ( element.textContent.toLowerCase() === item.textContent.toLowerCase() )
-            {
-              hasFilter = true
-              if(hasFilter) {
-                ulUstensils.removeChild(item);
-              }
-            } 
-          }) 
-        }
-      });
-    dropdownUstensilsList.appendChild(ulUstensils);
-    // Display message if no more filters disponible
-    if(ulUstensils.childElementCount === 0) {
-      const ustensilFail = document.createElement("p");
-      ustensilFail.classList.add("no-more-filter");
-      ustensilFail.textContent = "Plus aucun filtre ustensils disponible ..."
-      dropdownUstensilsList.removeChild(ulUstensils)
-      dropdownUstensilsList.appendChild(ustensilFail)
-    }
+    });
+  dropdownIngredientsList.appendChild(ulIngredients);
+  // Display message if no more filters disponible
+  if (ulIngredients.childElementCount === 0) {
+    const ingredientsFail = document.createElement("p");
+    ingredientsFail.classList.add("no-more-filter");
+    ingredientsFail.textContent = "Plus aucun filtre appareils disponible ...";
+    dropdownIngredientsList.removeChild(ulIngredients);
+    dropdownIngredientsList.appendChild(ingredientsFail);
   }
-  
-  // <---------- FUNCTIONS TO DISPLAY FILTERS LIST ---------->
-  function getIngredients(recipes) {
-    let ingredientsArr = [];
-    recipes.forEach((recipe) =>
-      recipe.ingredients.forEach((ingredient) =>
-        ingredientsArr.push(ingredient.ingredient)
-      )
-    );
-    return ingredientsArr;
+}
+
+//----- APPLIANCES -----
+function createAppliancesDropdown(
+  appliances,
+  currentFilter,
+  selectedFiltersContainer
+) {
+  const dropdownAppliancesList = document.querySelector(
+    ".appliances .filter-list"
+  );
+  const ulAppliances = document.createElement("ul");
+  appliances
+    .filter((appliance, index) => appliances.indexOf(appliance) === index)
+    .forEach((appliance) => {
+      const item = document.createElement("li");
+      item.setAttribute("onclick", "updateRecipes(event)");
+      item.textContent = appliance;
+      ulAppliances.appendChild(item);
+      // If a filter is selected, this filter is removed from the list
+      if (currentFilter) {
+        let hasFilter = false;
+        selectedFiltersContainer.querySelectorAll("span").forEach((element) => {
+          if (
+            element.textContent.toLowerCase() === item.textContent.toLowerCase()
+          ) {
+            hasFilter = true;
+            if (hasFilter) {
+              ulAppliances.removeChild(item);
+            }
+          }
+        });
+      }
+    });
+  dropdownAppliancesList.appendChild(ulAppliances);
+  // Display message if no more filters disponible
+  if (ulAppliances.childElementCount === 0) {
+    const applianceFail = document.createElement("p");
+    applianceFail.classList.add("no-more-filter");
+    applianceFail.textContent = "Plus aucun filtre appareils disponible ...";
+    dropdownAppliancesList.removeChild(ulAppliances);
+    dropdownAppliancesList.appendChild(applianceFail);
   }
-  
-  function getAppliances(recipes) {
-    let appliancesArr = [];
-    recipes.forEach((recipe) => appliancesArr.push(recipe.appliance));
-    return appliancesArr;
+}
+
+//----- USTENSILS -----
+function createUstensilsDropdown(
+  ustensils,
+  currentFilter,
+  selectedFiltersContainer
+) {
+  const dropdownUstensilsList = document.querySelector(
+    ".ustensils .filter-list"
+  );
+  const ulUstensils = document.createElement("ul");
+  ustensils
+    .filter((ustensil, index) => ustensils.indexOf(ustensil) === index)
+    .forEach((ustensil) => {
+      const item = document.createElement("li");
+      item.setAttribute("onclick", "updateRecipes(event)");
+      item.textContent = ustensil;
+      ulUstensils.appendChild(item);
+      // If a filter is selected, this filter is removed from the list
+      if (currentFilter) {
+        let hasFilter = false;
+        selectedFiltersContainer.querySelectorAll("span").forEach((element) => {
+          if (
+            element.textContent.toLowerCase() === item.textContent.toLowerCase()
+          ) {
+            hasFilter = true;
+            if (hasFilter) {
+              ulUstensils.removeChild(item);
+            }
+          }
+        });
+      }
+    });
+  dropdownUstensilsList.appendChild(ulUstensils);
+  // Display message if no more filters disponible
+  if (ulUstensils.childElementCount === 0) {
+    const ustensilFail = document.createElement("p");
+    ustensilFail.classList.add("no-more-filter");
+    ustensilFail.textContent = "Plus aucun filtre ustensils disponible ...";
+    dropdownUstensilsList.removeChild(ulUstensils);
+    dropdownUstensilsList.appendChild(ustensilFail);
   }
-  
-  function getUstensils(recipes) {
-    let ustensilsArr = [];
-    recipes.forEach((recipe) =>
-      recipe.ustensils.forEach((ustensil) => ustensilsArr.push(ustensil))
-    );
-    return ustensilsArr;
-  }
-  // ---------------------------------------------------------------------------------------
+}
+
+// <---------- FUNCTIONS TO DISPLAY FILTERS LIST ---------->
+function getIngredients(recipes) {
+  let ingredientsArr = [];
+  recipes.forEach((recipe) =>
+    recipe.ingredients.forEach((ingredient) =>
+      ingredientsArr.push(ingredient.ingredient)
+    )
+  );
+  return ingredientsArr;
+}
+
+function getAppliances(recipes) {
+  let appliancesArr = [];
+  recipes.forEach((recipe) => appliancesArr.push(recipe.appliance));
+  return appliancesArr;
+}
+
+function getUstensils(recipes) {
+  let ustensilsArr = [];
+  recipes.forEach((recipe) =>
+    recipe.ustensils.forEach((ustensil) => ustensilsArr.push(ustensil))
+  );
+  return ustensilsArr;
+}
+// ---------------------------------------------------------------------------------------
--- scripts/filters/filtersInputs.js
+++ scripts/filters/filtersInputs.js
@@ -1,46 +1,55 @@
+let filtersInputsUsed = false;
 // Functions for filters Inputs
 function filterIngredientsByInput(e) {
-    const inputValue = e.target.value;
+  filtersInputsUsed = true;
+  const inputValue = e.target.value;
+  filtersLists[0].innerHTML = "";
+  console.log(filtersInputsUsed);
+  if (inputValue.length >= 3) {
+    let ingredients = getIngredients(allRecipes);
+    allIngredients.splice(0, allIngredients.length);
     filtersLists[0].innerHTML = "";
-    if (inputValue.length >= 3) {
-      let ingredients = getIngredients(allRecipes)
-      for (const ingredient of ingredients) {
-        
+    for (const ingredient of ingredients) {
+      if (ingredient.toLowerCase().includes(e.target.value.toLowerCase())) {
+        allIngredients.push(ingredient);  
+        createIngredientsDropdown(allIngredients)
       }
-    } else {
-      createDropdownList(allRecipes)
     }
+  } else {
+    filtersInputsUsed = false;
+    createDropdownList(allRecipes);
   }
-  
-  function filterApplianceByInput(e) {
-    const inputValue = e.target.value.toLowerCase();
-    filtersLists[1].innerHTML = "";
-    if (inputValue.length >= 3) {
-      filteredRecipes = allRecipes.filter((recipe) =>
-        recipe.appliance.toLowerCase().includes(inputValue)
-      );
-      createDropdownList(filteredRecipes);
-    } else {
-      createDropdownList(allRecipes);
-    }
+}
+
+function filterApplianceByInput(e) {
+  const inputValue = e.target.value.toLowerCase();
+  filtersLists[1].innerHTML = "";
+  if (inputValue.length >= 3) {
+    filteredRecipes = allRecipes.filter((recipe) =>
+      recipe.appliance.toLowerCase().includes(inputValue)
+    );
+    createDropdownList(filteredRecipes);
+  } else {
+    createDropdownList(allRecipes);
   }
-  
-  function filterUstensilsByInput(e) {
-    const inputValue = e.target.value.toLowerCase();
-    cardContainer.innerHTML = "";
-    filtersLists.forEach((filtersList) => (filtersList.innerHTML = ""));
-    if (inputValue.length >= 3) {
-      filteredRecipes = allRecipes.filter(recipe => {
-        let hasUstensils = false;
-        recipe.ustensils.forEach(ustensil => {
-          if(ustensil.toLowerCase().includes(inputValue)) {
-            hasUstensils = true;
-          }
-        })
-        return hasUstensils
+}
+
+function filterUstensilsByInput(e) {
+  const inputValue = e.target.value.toLowerCase();
+  cardContainer.innerHTML = "";
+  filtersLists.forEach((filtersList) => (filtersList.innerHTML = ""));
+  if (inputValue.length >= 3) {
+    filteredRecipes = allRecipes.filter((recipe) => {
+      let hasUstensils = false;
+      recipe.ustensils.forEach((ustensil) => {
+        if (ustensil.toLowerCase().includes(inputValue)) {
+          hasUstensils = true;
+        }
       });
-      createDropdownList(filteredRecipes);
-    } else {
-      createDropdownList(allRecipes);
-    }
+      return hasUstensils;
+    });
+    createDropdownList(filteredRecipes);
+  } else {
+    createDropdownList(allRecipes);
   }
+}
