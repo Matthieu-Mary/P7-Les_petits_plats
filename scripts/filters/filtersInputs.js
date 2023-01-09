a75582da4b33c67f2c2d0e02481972f0e0da33bf
Matthieu Mary <matthieu.mary2@gmail.com>
2023-01-09 10:41:50 +0100

    Fix inputs bug

--- scripts/filters/filtersInputs.js
+++ scripts/filters/filtersInputs.js
@@ -2,7 +2,7 @@
 function filterIngredientsByInput(e) {
   const inputValue = e.target.value;
   filtersLists[0].innerHTML = "";
-  let ingredients = getIngredients(allRecipes);
+  let ingredients = getIngredients(filteredRecipes);
   allIngredients.splice(0, allIngredients.length);
   for (const ingredient of ingredients) {
     if (ingredient.toLowerCase().includes(inputValue.toLowerCase())) {
@@ -15,7 +15,7 @@ function filterIngredientsByInput(e) {
 function filterApplianceByInput(e) {
   const inputValue = e.target.value.toLowerCase();
   filtersLists[1].innerHTML = "";
-    let appliances = getAppliances(allRecipes);
+    let appliances = getAppliances(filteredRecipes);
     allAppliances.splice(0, allAppliances.length);
     for (const appliance of appliances) {
       if (appliance.toLowerCase().includes(inputValue.toLowerCase())) {
@@ -28,7 +28,7 @@ function filterApplianceByInput(e) {
 function filterUstensilsByInput(e) {
   const inputValue = e.target.value;
   filtersLists[2].innerHTML = "";
-  let ustensils = getUstensils(allRecipes);
+  let ustensils = getUstensils(filteredRecipes);
   allUstensils.splice(0, allUstensils.length);
   filtersLists[2].innerHTML = "";
   for (const ustensil of ustensils) {
