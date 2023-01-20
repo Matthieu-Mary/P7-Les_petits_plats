f330d1fd977ba406b962008d66bfe115d402b673
Matthieu Mary <matthieu.mary2@gmail.com>
2023-01-20 14:23:04 +0100

    like methods version

--- scripts/filters/filtersInputs.js
+++ scripts/filters/filtersInputs.js
@@ -8,13 +8,18 @@ let allUstensilsByInput = getUstensils(recipes);
 // Functions for filters Inputs
 function filterIngredientsByInput(e) {
   const inputValue = e.target.value;
+  let ingredients;
   if (inputValue.length !== 0) {
     filtersLists[0].innerHTML = "";
-    let ingredients;
-    if (search.value.length > 2) {
+    if (
+      search.value.length > 2 &&
+      selectedFiltersContainer.childElementCount === 0
+    ) {
       ingredients = getIngredients(filteredRecipesBySearch);
+    } else if (selectedFiltersContainer.childElementCount !== 0) {
+      ingredients = getIngredients(filteredRecipesByTags);
     } else {
-      ingredients = getIngredients(filteredRecipesByTags)
+      ingredients = getIngredients(recipes);
     }
     allIngredientsByInput.splice(0, allIngredientsByInput.length);
     for (const ingredient of ingredients) {
@@ -30,38 +35,52 @@ function filterIngredientsByInput(e) {
 }
 
 function filterApplianceByInput(e) {
-  const inputValue = e.target.value.toLowerCase();
-  filtersLists[1].innerHTML = "";
+  const inputValue = e.target.value;
   let appliances;
-  if (search.value.length > 2) {
-    appliances = getAppliances(filteredRecipesBySearch);
-  } else {
-    appliances = getAppliances(filteredRecipesByTags)
-  }
-  allAppliancesByInput.splice(0, allAppliancesByInput.length);
-  for (const appliance of appliances) {
-    if (appliance.toLowerCase().includes(inputValue.toLowerCase())) {
-      allAppliancesByInput.push(appliance);
-      createAppliancesDropdown(allAppliancesByInput);
+  if (inputValue.length !== 0) {
+    filtersLists[1].innerHTML = "";
+    if (search.value.length > 2) {
+      appliances = getAppliances(filteredRecipesBySearch);
+    } else if (selectedFiltersContainer.childElementCount !== 0) {
+      appliances = getAppliances(filteredRecipesByTags);
+    } else {
+      appliances = getAppliances(recipes);
     }
+    allAppliancesByInput.splice(0, allAppliancesByInput.length);
+    for (const appliance of appliances) {
+      if (appliance.toLowerCase().includes(inputValue.toLowerCase())) {
+        allAppliancesByInput.push(appliance);
+        createAppliancesDropdown(allAppliancesByInput);
+      }
+    }
+  } else {
+    allAppliancesByInput = getAppliances(filteredRecipesBySearch);
+    createAppliancesDropdown(allAppliancesByInput);
   }
 }
 
 function filterUstensilsByInput(e) {
   const inputValue = e.target.value;
-  filtersLists[2].innerHTML = "";
   let ustensils;
-  if (search.value.length > 2) {
-    ustensils  = getUstensils(filteredRecipesBySearch);
-  } else {
-    ustensils = getUstensils(filteredRecipesByTags);
-  }
-  allUstensilsByInput.splice(0, allUstensilsByInput.length);
-  filtersLists[2].innerHTML = "";
-  for (const ustensil of ustensils) {
-    if (ustensil.toLowerCase().includes(inputValue.toLowerCase())) {
-      allUstensilsByInput.push(ustensil);
-      createUstensilsDropdown(allUstensilsByInput);
+  if (inputValue.length !== 0) {
+    filtersLists[2].innerHTML = "";
+    if (search.value.length > 2) {
+      ustensils = getUstensils(filteredRecipesBySearch);
+    } else if (selectedFiltersContainer.childElementCount !== 0) {
+      ustensils = getUstensils(filteredRecipesByTags);
+    } else {
+      ustensils = getUstensils(recipes);
     }
+    allUstensilsByInput.splice(0, allUstensilsByInput.length);
+    filtersLists[2].innerHTML = "";
+    for (const ustensil of ustensils) {
+      if (ustensil.toLowerCase().includes(inputValue.toLowerCase())) {
+        allUstensilsByInput.push(ustensil);
+        createUstensilsDropdown(allUstensilsByInput);
+      }
+    }
+  } else {
+    allUstensilsByInput = getUstensils(filteredRecipesBySearch);
+    createUstensilsDropdown(allUstensilsByInput);
   }
 }
